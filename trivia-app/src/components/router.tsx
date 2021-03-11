3f6178cfb44936652f308ee39d68545d990b6104
Cynth <cynthia.enciso28@gmail.com>
2021-03-11 14:40:56 -0600

    merge again

--- trivia-app/src/components/router.tsx
+++ trivia-app/src/components/router.tsx
@@ -5,34 +5,12 @@ import Login from "./login";
 import Game from "./game";
 
 export default function RouterComponent() {
-<<<<<<< HEAD
-
-||||||| 97edd4d
-    const location = useLocation();
-=======
->>>>>>> 9e6f057bf7303d67c562d40fb26b29994930000b
     
     return (
-<<<<<<< HEAD
-        <Switch>
-            <Route exact path="/" render= { () => <Redirect to="/login" />} />
-            <Route path="/login" component={Login} />
-            <Route path="/game" component={Game} />
-        </Switch>
-||||||| 97edd4d
-        <ErrorBoundaryComponent key={location.pathname}>
             <Switch>
                 <Route exact path="/" render= { () => <Redirect to="/login" />} />
                 <Route path="/login" component={Login} />
                 <Route path="/game" component={Game} />
             </Switch>
-        </ErrorBoundaryComponent>
-=======
-            <Switch>
-                <Route exact path="/" render= { () => <Redirect to="/login" />} />
-                <Route path="/login" component={Login} />
-                <Route path="/game" component={Game} />
-            </Switch>
->>>>>>> 9e6f057bf7303d67c562d40fb26b29994930000b
     );
 }
